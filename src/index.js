267fcca84d2096fc2a8b116f257fe1efbdd44947
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-11 08:21:33 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 // Code Goes Here
 import React from 'react';
 import PropTypes from 'prop-types';
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      <h1>Hello, {this.props.name}</h1>
+    );
+  }
+}
+
+Greeting.propTypes = {
+  name: PropTypes.string
+};
